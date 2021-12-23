caae9fb2565bb7bb04a7d664925dcc00e4376726
deepgandhecha7 <deepgandhecha7@gmail.com>
2021-12-23 11:08:57 +0530

    Assignment 3 done

--- Assignment-3/code/src/App.js
+++ Assignment-3/code/src/App.js
@@ -1,15 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
-
-var arrow="<"
+import data from './Data'
+let arrow="<";
 function App() {
   return (
-    <div className="site-container">
-    {/* Start Your code here */}
+    data.map((val)=>{
+      return(
+      <div className="site-container">
+        <div className="customer-info">
+          <p className="arrow">{arrow}</p>
+          <div className="top-text">
+            <p><b>{val.Name}</b></p>
+            <p className="id">{val.Id}</p>
+          </div>
+          <div className="btn">
+          <input type="Submit" value="Print" className="button"/>
+          </div>
+      </div>
+      <div className="card2">
+        <p><b>Appointment:</b><span>&nbsp;&nbsp;{val.Appointment}</span></p>
+        <p><b>Email :</b><span>&nbsp;&nbsp;{val.Email}</span></p>
+        <p><b>Phone:</b><span>&nbsp;&nbsp;{val.phone}</span></p>
+      </div>
+      <div className="order-info">
+        <div>
+          <ul className="list">
+            <li><b>Status</b></li>
+            <li><b>Door</b></li>
+            <li><b>Time</b></li>
+          </ul>
+        </div>
+        <div>
+          <ul className="listval">
+          <li>{val.Status}</li>
+          <li>{val.Door}</li>
+          <li>{val.Time}&nbsp;<span className="sp">({val.Date})</span></li>
+          </ul>
+        </div>
+      </div>
+      <div className="product-list">
+        <div>
+          <input type="checkbox"/>
+        </div>
+        <div>
+        <img src={val.Image} alt="person"/>
+        </div>
+        <div>
+          <p><b>{val.productName}</b></p>
+          <p>{val.productDetail}</p>
+        </div>
+      </div>
     </div>
-  );
+  )}));
 }
-
 export default App;
---
+++ Assignment-3/code/src/Data.js
@@ -0,0 +1,17 @@
+import img from './img.jpg' 
+const data=[{
+        Name:"Alan Ford",
+        Email:"alan.form@email.com",
+        Image:img,
+        Status:"In Progress",
+        Door:"Mark",
+        Time:"10:30",
+        Date:"25-05-2016",
+        phone:"+1234567890",
+        Appointment:"9:00 (24-05-2016)",
+        Id:"00005152",
+        productName:"Boltaart Bosbessen",
+        productDetail:"Lorem Ipsum is simply dummy text of the printing"
+}
+];
+export default data;
